1b69d173c1f3f43d77cb3e0ae40a257a2d0db6d6
A8 <ryouta.pooh@icloud.com>
2026-08-14 23:20:20 +0900

    Laser Core Lang追加

--- src/main/java/moremultiblock/common/MMBLang.java
+++ src/main/java/moremultiblock/common/MMBLang.java
@@ -7,9 +7,14 @@ import net.minecraft.Util;
 public enum MMBLang implements ILangEntry {
     RADIOACTIVE_WASTE_VAULT("gui", "radioactive_waste_vault"),
     DESCRIPTION_RADIOACTIVE_WASTE_VAULT("description", "radioactive_waste_vault"),
+    DESCRIPTION_RADIOACTIVE_WASTE_VALVE("description", "radioactive_waste_valve"),
 
-    RADIOACTIVE_WASTE_VALVE("gui", "radioactive_waste_valve"),
-    DESCRIPTION_RADIOACTIVE_WASTE_VALVE("description", "radioactive_waste_valve");
+    LASER_CORE("laser_core","laser_core"),
+    DESCRIPTION_LASER_CORE_CASING("description","laser_core_casing"),
+    DESCRIPTION_LASER_CORE_PORT("description","laser_core_port"),
+    DESCRIPTION_LASER_CORE("description","laser_core")
+    ;
+
 
     private final String key;
 
